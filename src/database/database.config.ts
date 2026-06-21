31065df55ea0be8ad83b84a436a2f4c663d64a13
SRT DEV <sahirruiz4@gmail.com>
2026-06-20 19:21:41 -0500

    refactor(database): mejorar la estructura de configuración de la base de datos

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,21 +1,24 @@
 import { User } from '../modules/users/entities/user.entity';
 import { Ticket } from '../modules/tickets/entities/ticket.entity';
 
-export const databaseConfig = () => ({
-  type: 'postgres' as const,
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+export const databaseConfig = () => {
+  console.log('DATABASE_HOST=', process.env.DATABASE_HOST);
+  console.log('DATABASE_PORT=', process.env.DATABASE_PORT);
 
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
-
-  entities: [User, Ticket],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  migrationsRun: true,
-  synchronize: false,
-});
+  return {
+    type: 'postgres' as const,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+    entities: [User, Ticket],
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
+    migrationsRun: true,
+    synchronize: false,
+  };
+};
